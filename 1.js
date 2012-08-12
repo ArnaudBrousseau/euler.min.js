7ee4ba7216c934405c5949a77bfbd7e5a03d883f
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2012-08-11 20:15:13 -0700

    Problem 1 - separated the minified from the standard version to keep track of the main logic. This will help further down the road.

--- 1.js
+++ 1.js
@@ -1,7 +1,16 @@
-i=r=0
-for (i, i<1000, i++) {
-  if (!(i % 3) || !(i % 5)) {
-    r += i;
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+var i = 0;
+var result = 0;
+
+for (i=0; i < 1000; i++) {
+  if (i % 3 === 0 || i % 5 ===0) {
+    result += i;
   }
 }
-console.log(r)
+
+console.log(result);
+
